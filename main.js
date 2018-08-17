d001417af688aa6864cb4565e516aea8fe6973d9
momo5502 <mail@momo5502.com>
2018-08-17 13:55:10 +0200

    Better color generation

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 window.addEventListener("load", run);
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
 function run() {
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext('2d');
@@ -8,17 +12,21 @@ function run() {
     var colors = parseInt(Math.random() * 3) + 2;
 
     for (var i = 0; i < colors; ++i) {
-        gradient.push("hsl(" + Math.random() * 360 + ", 100%," + (((colors - (i + 1)) * 70) / colors) + "%)");
+        var hue = getRandomInt(0, 360);
+        var saturation = getRandomInt(30, 100);
+        var lightness = getRandomInt(((colors - (i + 1)) * 70) / colors, ((colors - (i)) * 70) / colors);
+        gradient.push("hsl(" + hue + ", " + saturation + "%," + lightness + "%)");
     }
 
     var triangles = parseInt(Math.random() * 100) % 2 == 0;
+    var colorFromLeft = parseInt(Math.random() * 100) % 2 == 0;
 
     var polygonRenderer = new PolygonRenderer(context, {
         generateTriangles: triangles,
         distortionScale: triangles ? 0.25 : 0.45,
-        polygonDimensions: triangles ? [100, 100] : [80, 80],
+        polygonDimensions: triangles ? [120, 100] : [80, 80],
         gradient: gradient,
-        leftColor: parseInt(Math.random() * 100) % 2 == 0
+        leftColor: colorFromLeft
     });
 
     var render = (loop) => {
--- polygon.js
+++ polygon.js
@@ -85,6 +85,7 @@ class PolygonRenderer {
 
         this.ctx.strokeStyle = gr;
         this.ctx.fillStyle = gr;
+        this.ctx.lineWidth = 2;
         this.ctx.closePath();
         this.ctx.fill();
         this.ctx.stroke();
@@ -112,8 +113,8 @@ class PolygonRenderer {
         var height = this.ctx.canvas.height;
         var width = this.ctx.canvas.width;
 
-        var yElements = parseInt(height / yStep) + 3;
-        var xElements = parseInt(width / xStep) + 3;
+        var yElements = parseInt(height / yStep) + 2;
+        var xElements = parseInt(width / xStep) + 2;
 
         this.vertices = new Array(xElements * yElements);
         this.polygons = [];
